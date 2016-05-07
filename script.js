c91ee4cc97d10b6b63e1b8934c9e07588d9d7d7f
Ryan P.C. McQuen <ryan.q@linux.com>
2016-05-07 11:10:55 -0700

    Use remove version of minus; more tweaking.
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- script.js
+++ script.js
@@ -1,55 +1,3 @@
-/*! minus.js v0.3.0 by ryanpcmcquen */
-(function () {
-  'use strict';
-  var m = {};
-  m.d = document;
-  m.o = Object;
-  m.ap = Array.prototype;
-  m.eq = function (e, s) {
-    return e.querySelector(s);
-  };
-  m.eqa = function (e, s) {
-    return e.querySelectorAll(s);
-  };
-  m.q = function (s) {
-    return m.eq(m.d, s);
-  };
-  m.qa = function (s) {
-    return m.eqa(m.d, s);
-  };
-  m.ael = function (s, e, c) {
-    return s.addEventListener(e, c);
-  };
-  m.oc = function (p, o) {
-    return m.o.create(p, o);
-  };
-  m.ok = function (o) {
-    return m.o.keys(o);
-  };
-  m.apsc = function (a, s, e) {
-    s = s || 0;
-    e = e || undefined;
-    return m.ap.slice.call(a, s, e);
-  };
-  m.qs = m.q;
-  m.qsa = m.qa;
-  m.eqs = m.eq;
-  m.eqsa = m.eqa;
-  var infoLog = function infoLog(o) {
-    return console.info("You now have `minus.js` shortcuts under the global object `" + o + "`.");
-  };
-  if (!window.m) {
-    window.m = m;
-    infoLog("m");
-  } else if (!window.mi) {
-    window.mi = m;
-    infoLog("mi");
-  } else {
-    console.warn("You already have global objects named `m` & `mi`. \n `minus.js` has gracefully shown itself the door.");
-  }
-  return m;
-}());
-
 /*jslint browser:true, white:true*/
 (function () {
 
@@ -78,9 +26,9 @@
 
   var updateCalculations;
   updateCalculations = function () {
-    // always times money calculations by 100, because
-    // javascript sucks at decimals, and people like money
-    // calculations to be accurate
+    // Always times money calculations by 100, because
+    // JavaScript sucks at decimals, and people like money
+    // calculations to be accurate.
     mortgage_paid_in_full.textContent = (((monthly_mortgage_payment * 100 * 12) * years_left) / 100);
 
 
@@ -95,8 +43,9 @@
     var year_ten_maint_calc;
     year_ten_maint_calc = function () {
       var final_maintenance_fee = 0;
-      final_maintenance_fee = (parseFloat(annual_maintenance_fees) * Math.pow(1 + annual_maintenance_increase, 9));
+      final_maintenance_fee = (parseFloat(annual_maintenance_fees) * Math.pow((1 + annual_maintenance_increase), 9));
       year_ten_maintenance_fee.textContent = final_maintenance_fee;
+      console.log(final_maintenance_fee);
     };
     year_ten_maint_calc();
 
